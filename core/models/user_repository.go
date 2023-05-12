a2245ee40fcc685041cb605cf765f006226fb2a3
wanghengjing <982246809@qq.com>
2023-05-12 19:42:31 +0800

    11

--- core/models/user_repository.go
+++ core/models/user_repository.go
@@ -1,1 +1,20 @@
 package models
+
+import "time"
+
+type UserRepository struct {
+	Id                 int
+	Identity           string
+	UserIdentity       string
+	Name               string
+	Ext                string
+	ParentId           int64
+	RepositoryIdentity string
+	CreatedAt          time.Time `xorm:"created"`
+	UpdatedAt          time.Time `xorm:"updated"`
+	DeletedAt          time.Time `xorm:"deleted"`
+}
+
+func (table UserRepository) TableName() string {
+	return "user_repository"
+}
